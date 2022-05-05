9ba833bc56d2b1ccb44f1dfd441a49b885ea8bdd
joNA <jitzian@gmail.com>
2022-05-04 20:11:01 -0400

    nyc jc - Initial commit for Detail VM

---
+++ app/src/main/java/com/example/nyccompose/schools/detail/viewmodel/DetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.nyccompose.schools.detail.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel : ViewModel() {
+    private val TAG = DetailViewModel::class.java.simpleName
+
+}
