b84ccc2c6dc9d5788c2a8fd3b7c50e5ccdf9dd80
joNA <jitzian@gmail.com>
2022-05-15 22:30:26 -0400

    nyc jc - First iteration for Loading Screen

---
+++ app/src/main/java/com/example/nyccompose/ui/common/loading/LoadingScreeen.kt
@@ -0,0 +1,51 @@
+package com.example.nyccompose.ui.common.loading
+
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nyccompose.R
+
+@Composable
+@Preview(showBackground = true)
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val progressValue = 0.75f
+        val infiniteTransition = rememberInfiniteTransition()
+
+        val progressAnimationValue by infiniteTransition.animateFloat(
+            initialValue = 0.0f,
+            targetValue = progressValue,
+            animationSpec = infiniteRepeatable(animation = tween(900))
+        )
+
+        CircularProgressIndicator(progress = progressAnimationValue)
+
+        Text(
+            text = stringResource(id = R.string.loading_dot_dot_dot_TEXT),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = dimensionResource(id = R.dimen.dimen_8_dp)),
+            style = MaterialTheme.typography.caption,
+            textAlign = TextAlign.Center
+        )
+    }
+}
