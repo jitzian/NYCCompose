b563ef5ee9354df7c3cbace2664df2d5eb9bfd1e
joNA <jitzian@gmail.com>
2022-05-17 20:00:58 -0400

    nyc jc - Some cleanup

--- app/src/main/java/com/example/nyccompose/MainActivity.kt
+++ app/src/main/java/com/example/nyccompose/MainActivity.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import coil.annotation.ExperimentalCoilApi
-import com.example.nyccompose.schools.main.view.MainSchoolScreenState
+import com.example.nyccompose.ui.app.NYCApp
 import com.example.nyccompose.ui.navigation.NavigationConfig
-import com.example.nyccompose.ui.theme.NYCComposeTheme
 
 @ExperimentalCoilApi
 @ExperimentalFoundationApi
@@ -24,29 +16,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NYCComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    //Greeting("Android")
-                    NavigationConfig()
-                }
+            NYCApp {
+                NavigationConfig()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NYCComposeTheme {
-        Greeting("Android")
     }
 }
